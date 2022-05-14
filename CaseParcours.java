2898050ef2aafd2ff0fb330ae958af7911f7d4a1
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-05-14 16:58:02 +0100

    test

--- CaseParcours.java
+++ CaseParcours.java
@@ -4,6 +4,7 @@ public class CaseParcours extends Case {
   @Override
   public String getCouleur() {
     // TODO Auto-generated method stub
+    System.out.println("hello");
     return this.couleur;
   }
 }
