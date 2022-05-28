994a8ad04b355d2b0806b66db28bc4ecdbdc29fb
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-05-28 01:00:22 +0100

    g

--- Partie.java
+++ Partie.java
@@ -3,52 +3,29 @@ public class Partie {
   private int indice_actuel;
   private Plateau plateau;
 
-  /**
-   * 
-   */
   public Partie() {
-    // traitement de fichier pour avoir le score actuel
-    // traitement de fichier pour avoir l'indice actuel
   }
 
-  /**
-   * @return the plateau
-   */
   public Plateau getPlateau() {
     return plateau;
   }
 
-  /**
-   * @param plateau the plateau to set
-   */
   public void setPlateau(Plateau plateau) {
     this.plateau = plateau;
   }
 
-  /**
-   * @return the score_actuel
-   */
   public int getScore_actuel() {
     return score_actuel;
   }
 
-  /**
-   * @param score_actuel the score_actuel to set
-   */
   public void setScore_actuel(int score_actuel) {
     this.score_actuel = score_actuel;
   }
 
-  /**
-   * @return the indice_actuel
-   */
   public int getIndice_actuel() {
     return indice_actuel;
   }
 
-  /**
-   * @param indice_actuel the indice_actuel to set
-   */
   public void setIndice_actuel(int indice_actuel) {
     this.indice_actuel = indice_actuel;
   }
--- QuestionDefinition.java
+++ QuestionDefinition.java
@@ -1,3 +1,12 @@
 public class QuestionDefinition extends CaseQuestion {
+  private String definition;
+  private String reponse;
+  private int long_rep;
+
+  public QuestionDefinition(String definition, String reponse) {
+    this.definition = definition;
+    this.reponse = reponse;
+    this.long_rep = reponse.length();
+  }
 
 }
