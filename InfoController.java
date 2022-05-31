6cd39779587c432158a3c890f62f9071a062c3f3
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-05-31 23:08:57 +0100

    red circle done

--- InfoController.java
+++ InfoController.java
@@ -2,6 +2,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -35,18 +36,46 @@ public class InfoController {
     public void setPlateau(Joueur joueur) {
         for (Node node : plateau.getChildren()) {
             int indice = Integer.parseInt(((Button) node).getText());
-            node.setStyle("-fx-background-color:" + joueur.partie.getPlateau().getTab_cases()[indice].getCouleur());
+            node.setStyle("-fx-background-color:" + joueur.partie.getPlateau().getTab_cases()[indice].getCouleur()
+                    + ";-fx-content-display: center;");
+            node.setOnMouseClicked(event -> {
+                for (Node j : plateau.getChildren()) {
+                    if (j.equals(node)) {
+                        Image image = new Image(getClass().getResource("./assets/RedCircle.png").toExternalForm());
+                        ImageView icon = new ImageView(image);
+                        ((Button) node).setGraphic(icon);
+                    } else {
+                        Image image = new Image(getClass().getResource("./assets/RedCircle.png").toExternalForm());
+                        ImageView icon = new ImageView(image);
+                        icon.setVisible(false);
+                        ((Button) j).setGraphic(icon);
+
+                    }
+                }
+            });
         }
     }
 
-    public void deroulement_jeu(Joueur joueur) {
-        int indice = 0;
-        int found = 0;
+    public Button Recherche(int indice) {
+        Button found = new Button();
         for (Node node : plateau.getChildren()) {
             if (((Button) node).getText().equals("" + indice)) {
-                found = Integer.parseInt(((Button) node).getText());
+                found = (Button) node;
             }
         }
+        return found;
+    }
+
+    public void deroulement_jeu(Joueur joueur) {
+        // while (joueur.partie.getIndice_actuel() < 99) {
+        // Button Btn = Recherche(joueur.partie.getIndice_actuel());
+        // Btn.setStyle("-fx-background-color:" +
+        // joueur.partie.getPlateau().getTab_cases()[joueur.partie
+        // .getIndice_actuel()].getCouleur()
+        // + ";-fx-border-color:black" +
+        // ";-fx-border-width: 5");
+        // joueur.partie.getPlateau().getTab_cases()[joueur.partie.getIndice_actuel()].ActionCase(joueur.partie);
+        // }
     }
 
 }
